0fd7b2fd670329c3c2aa1b9c69ff40002b37c7d6
Pranavv Raja <114171126+pranavvraja@users.noreply.github.com>
2024-06-16 03:25:12 +0530

    basic layout in events and projects

---
+++ src/app/events/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Events = (props: Props) => {
+  return <div>page</div>;
+};
+
+export default Events;
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Projects = (props: Props) => {
+  return <div>page</div>;
+};
+
+export default Projects;
