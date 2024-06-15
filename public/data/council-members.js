942d1e1f7150f8d5a728aa2cdf149d57c7f7b5db
Pranavv Raja <114171126+pranavvraja@users.noreply.github.com>
2024-06-16 03:03:40 +0530

    created council and sample members data

---
+++ public/data/council-members.js
@@ -0,0 +1,74 @@
+export const council = [
+  {
+    id: 1,
+    name: "Vyasa M Nayak",
+    designation: "President",
+    github_username: "septicalls",
+    quote: "Real programmers count from 0.",
+  },
+  {
+    id: 2,
+    name: "Nihaal Y K",
+    designation: "Vice president",
+    github_username: "Nihaalyk",
+    quote: "One man's constant is another man's variable.",
+  },
+  {
+    id: 3,
+    name: "Sharon Tyana Menezes",
+    designation: "Secretary",
+    github_username: "Sharontm",
+    quote: "There’s no place like 127.0.0.1.",
+  },
+  {
+    id: 4,
+    name: "Charis Pinto",
+    designation: "Technical coordinator",
+    github_username: "Charispinto",
+    quote:
+      "Programmers are not to be measured by their ingenuity and their logic but by the completeness of their case analysis.",
+  },
+  {
+    id: 5,
+    name: "Lawrence Robert D Souza",
+    designation: "Membership coordinator",
+    github_username: "lawrencedsz9",
+    quote: "The best way to get a project done faster is to start sooner.",
+  },
+  {
+    id: 6,
+    name: "Rachan Karkera",
+    designation: "Media coordinator",
+    github_username: "rachan2005",
+    quote: "First, solve the problem. Then, write the code.",
+  },
+  {
+    id: 7,
+    name: "Deric Jojo",
+    designation: "App development lead",
+    github_username: "JackitudilinksG",
+    quote: "Programming is thinking, not typing.",
+  },
+  {
+    id: 8,
+    name: "Pranavv Raja C P",
+    designation: "Web development lead",
+    github_username: "pranavvraja",
+    quote: "Code is like humor. When you have to explain it, it’s bad.",
+  },
+  {
+    id: 9,
+    name: "Pratham V Bhat",
+    designation: "Co-founder",
+    github_username: "prathamvbhat",
+    quote:
+      "The best thing about a boolean is even if you are wrong, you are only off by a bit.",
+  },
+  {
+    id: 10,
+    name: "Omkar Bhat",
+    designation: "Co-founder",
+    github_username: "OmkarBhat08",
+    quote: "Weeks of programming can save you hours of planning.",
+  },
+];
---
+++ public/data/sample-members.js
@@ -0,0 +1,172 @@
+export const members = [
+  {
+    id: 1,
+    github_username: "alriyadsouza",
+    name: "Alriya Treeza D Souza",
+    role: "Member",
+    quote:
+      "There are 10 types of people in this world: those who understand binary and those who don’t.",
+  },
+  {
+    id: 2,
+    github_username: "anishshobithps",
+    name: "Anish Shobith P S",
+    role: "Member",
+    quote:
+      "I would love to change the world, but they won’t give me the source code.",
+  },
+  {
+    id: 3,
+    github_username: "Charispinto",
+    name: "Charis",
+    role: "Owner",
+    quote:
+      "The best thing about a boolean is even if you are wrong, you are only off by a bit.",
+  },
+  {
+    id: 4,
+    github_username: "JackitudilinksG",
+    name: "Deric Jojo",
+    role: "Member",
+    quote: "It’s not a bug – it’s an undocumented feature.",
+  },
+  {
+    id: 5,
+    github_username: "JoelVCrasta",
+    name: "Joel Crasta",
+    role: "Member",
+    quote: "Programmers are tools for converting caffeine into code.",
+  },
+  {
+    id: 6,
+    github_username: "JoshuaAlbuquerque",
+    name: null,
+    role: "Owner",
+    quote:
+      "In order to understand recursion, one must first understand recursion.",
+  },
+  {
+    id: 7,
+    github_username: "joywin2003",
+    name: "Joywin Bennis",
+    role: "Member",
+    quote: "To iterate is human, to recurse divine.",
+  },
+  {
+    id: 8,
+    github_username: "lawrencedsz9",
+    name: null,
+    role: "Member",
+    quote: "The computer was born to solve problems that did not exist before.",
+  },
+  {
+    id: 9,
+    github_username: "MacDsouza",
+    name: "Mac Dsouza",
+    role: "Member",
+    quote: "If at first you don’t succeed; call it version 1.0.",
+  },
+  {
+    id: 10,
+    github_username: "MainKt",
+    name: "Muhammad Saheed",
+    role: "Member",
+    quote: "Computers are fast; programmers keep it slow.",
+  },
+  {
+    id: 11,
+    github_username: "nagarajpurandar",
+    name: null,
+    role: "Member",
+    quote:
+      "There are two ways to write error-free programs; only the third one works.",
+  },
+  {
+    id: 12,
+    github_username: "OmkarBhat08",
+    name: "Omkar Bhat",
+    role: "Member",
+    quote: "It’s not a bug – it’s an undocumented feature.",
+  },
+  {
+    id: 13,
+    github_username: "Pranaam19",
+    name: "Pranaam19",
+    role: "Member",
+    quote: "Hardware: The parts of a computer that can be kicked.",
+  },
+  {
+    id: 14,
+    github_username: "pranavvraja",
+    name: "Pranavv Raja",
+    role: "Owner",
+    quote: "Weeks of programming can save you hours of planning.",
+  },
+  {
+    id: 15,
+    github_username: "Rishal14",
+    name: "Rishal D",
+    role: "Member",
+    quote: "Debugging: Removing the needles from the haystack.",
+  },
+  {
+    id: 16,
+    github_username: "samwinp",
+    name: "samwin steve pereira",
+    role: "Member",
+    quote:
+      "Software and cathedrals are much the same – first we build them, then we pray.",
+  },
+  {
+    id: 17,
+    github_username: "septicalls",
+    name: "Vyasa M Nayak",
+    role: "Owner",
+    quote:
+      "Programmers don’t burn out on hard work, they burn out on change-with-the-wind directives and not 'shipping'.",
+  },
+  {
+    id: 18,
+    github_username: "Sharontm",
+    name: "Sharon Tyana Menezes",
+    role: "Member",
+    quote: "There’s no place like 127.0.0.1.",
+  },
+  {
+    id: 19,
+    github_username: "Swastikb794",
+    name: "Swastik Bhandarkar",
+    role: "Member",
+    quote:
+      "A good programmer is someone who always looks both ways before crossing a one-way street.",
+  },
+  {
+    id: 20,
+    github_username: "UthpalSuvarna",
+    name: "Uthpal ",
+    role: "Member",
+    quote: "Real programmers count from 0.",
+  },
+  {
+    id: 21,
+    github_username: "vv-01",
+    name: "A S Vijayavitthala",
+    role: "Member",
+    quote: "One man’s crappy software is another man’s full-time job.",
+  },
+  {
+    id: 22,
+    github_username: "Vyshnav001",
+    name: "Vyshnav",
+    role: "Member",
+    quote:
+      "Software undergoes beta testing shortly before it’s released. Beta is Latin for 'still doesn’t work'.",
+  },
+  {
+    id: 23,
+    github_username: "Vyasa M Nayak",
+    role: "",
+    quote:
+      "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  },
+];
