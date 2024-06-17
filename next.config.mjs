699a973cf4412faebea094ba0b60da8a24fedbd1
Pranavv Raja <114171126+pranavvraja@users.noreply.github.com>
2024-06-17 15:36:43 +0530

    configured image host in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sceptix-club.github.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
