445d0ea1e0f64d0146a876e37fb4c223bb54f9cb
Pranavv Raja <114171126+pranavvraja@users.noreply.github.com>
2024-06-18 21:28:08 +0530

    domain branch first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,20 @@ import Image from "next/image";
 // import { Domain } from "@/components/sections/Domain";
 import { LayoutGridDemo } from "@/components/LayoutGrid";
 import Hero from "@/components/sections/Hero";
+import { DockDemo } from "@/components/DockDemo";
+import Beam from "@/components/Beam";
 
 export default function Home() {
   return (
     <>
-      <div className="h-screen">
-        <Hero />
-      </div>
+      <div className="h-screen">{/* <Hero /> */}</div>
       <div className="h-screen">About Us</div>
       <div className="h-screen">
         <h1>Domains</h1>
         {/* <Domain /> */}
-        <LayoutGridDemo />
+        {/* <LayoutGridDemo /> */}
+        <Beam />
+        <DockDemo />
       </div>
     </>
   );
---
+++ src/components/magicui/dock.tsx
@@ -0,0 +1,116 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
+import React, { PropsWithChildren, useRef } from "react";
+
+export interface DockProps extends VariantProps<typeof dockVariants> {
+  className?: string;
+  magnification?: number;
+  distance?: number;
+  children: React.ReactNode;
+}
+
+const DEFAULT_MAGNIFICATION = 110;
+const DEFAULT_DISTANCE = 140;
+
+const dockVariants = cva(
+  "mx-auto min-w-min w-1/3  h-1/6 min-h-min p-2 flex justify-center items-end gap-5 rounded-2xl font-fira_sans"
+);
+
+const Dock = React.forwardRef<HTMLDivElement, DockProps>(
+  (
+    {
+      className,
+      children,
+      magnification = DEFAULT_MAGNIFICATION,
+      distance = DEFAULT_DISTANCE,
+      ...props
+    },
+    ref
+  ) => {
+    const mouseX = useMotionValue(Infinity);
+
+    const renderChildren = () => {
+      return React.Children.map(children, (child: any) => {
+        return React.cloneElement(child, {
+          mouseX: mouseX,
+          magnification: magnification,
+          distance: distance,
+        });
+      });
+    };
+
+    return (
+      <motion.div
+        ref={ref}
+        onMouseMove={(e) => mouseX.set(e.pageX)}
+        onMouseLeave={() => mouseX.set(Infinity)}
+        {...props}
+        className={cn(dockVariants({ className }), className)}
+      >
+        {renderChildren()}
+      </motion.div>
+    );
+  }
+);
+
+Dock.displayName = "Dock";
+
+export interface DockIconProps {
+  size?: number;
+  magnification?: number;
+  distance?: number;
+  mouseX?: any;
+  className?: string;
+  children?: React.ReactNode;
+  props?: PropsWithChildren;
+}
+
+const DockIcon = ({
+  size,
+  magnification = DEFAULT_MAGNIFICATION,
+  distance = DEFAULT_DISTANCE,
+  mouseX,
+  className,
+  children,
+  ...props
+}: DockIconProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const distanceCalc = useTransform(mouseX, (val: number) => {
+    const bounds = ref.current?.getBoundingClientRect() ?? { x: 0, width: 0 };
+
+    return val - bounds.x - bounds.width / 2;
+  });
+
+  let widthSync = useTransform(
+    distanceCalc,
+    [-distance, 0, distance],
+    [80, magnification, 80]
+  );
+
+  let width = useSpring(widthSync, {
+    mass: 0.9,
+    stiffness: 250,
+    damping: 10,
+  });
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{ width }}
+      className={cn(
+        "flex aspect-square cursor-pointer items-center justify-center rounded-full bg-neutral-400/40 ",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+DockIcon.displayName = "DockIcon";
+
+export { Dock, DockIcon, dockVariants };
