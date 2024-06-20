8634ec00be74d3f8113967056d9dd9e3b8e29c4e
vyshnav <vyshnavravi599@gmail.com>
2024-06-20 21:05:08 +0530

    feat: Update Events page to display EventCard components

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+"use client"
+import EventCard from "@/components/Shared/EventCard";
 
-type Props = {};
-
-const Events = (props: Props) => {
-  return <div>page</div>;
-};
-
-export default Events;
+const Events = ()=> {
+  const eventCards = Array.from({ length: 10 });
+  return (
+    <div className="p-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 mt-1">
+      {
+        
+        eventCards.map((_,index)=>(
+          <EventCard key={index}/>
+        ))
+        
+      }
+    </div>
+  );
+}
+export default Events
