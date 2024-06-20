77119441cec186c0767c2822ed13a80110e93f3b
Pranavv Raja <114171126+pranavvraja@users.noreply.github.com>
2024-06-20 18:30:04 +0530

    feat:hero section

--- src/components/FlipWordsHero.tsx
+++ src/components/FlipWordsHero.tsx
@@ -6,7 +6,7 @@ export function FlipWordsHero() {
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
-      <div className="text-8xl mx-auto font-fira_sans text-neutral-600 font-extrabold dark:text-neutral-400">
+      <div className="md:text-9xl text-7xl text-center mx-auto font-fira_sans text-neutral-600 font-extrabold dark:text-neutral-400">
         The <FlipWords words={words} /> club
       </div>{" "}
     </div>
