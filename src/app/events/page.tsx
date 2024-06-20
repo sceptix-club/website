e908073ea232546d57d0ec2f9a1ded82be95b276
vyshnav <vyshnavravi599@gmail.com>
2024-06-21 00:35:30 +0530

    refactor: Optimize EventCard component and remove unused code

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,18 +1,30 @@
 "use client"
 import EventCard from "@/components/Shared/EventCard";
+import {events} from  "@/data"
+import {motion} from "framer-motion"
 
 const Events = ()=> {
-  const eventCards = Array.from({ length: 10 });
   return (
+    <>
+    <div className="flex justify-center items-center w-full h-20">
+      <motion.h1 className="md:text-5xl text-3xl font-extrabold mt-5 md:ml-5 justify-center text-center font-fira_sans" 
+      initial={{opacity:0 , y:-100}}
+      animate={{opacity:1,y:0}}
+      transition={{duration:0.5}}
+      >
+        Events
+      </motion.h1>
+    </div>
     <div className="p-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 mt-1">
       {
         
-        eventCards.map((_,index)=>(
-          <EventCard key={index}/>
+        events.map((events,index)=>(
+          <EventCard key={index} {...events} />
         ))
         
       }
     </div>
+    </>
   );
 }
 export default Events
