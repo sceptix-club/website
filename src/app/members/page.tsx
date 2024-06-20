bdcd3eaabd3f3238a0f972802f985bb8fc40d445
Pranavv Raja <114171126+pranavvraja@users.noreply.github.com>
2024-06-20 11:28:05 +0530

    update:members page

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,30 +1,32 @@
 import { MemberCard } from "@/components/MemberCard";
 import React from "react";
-import {council , members} from "@/data/members"
-import { CouncilMemberType,MemberType } from "@/types";
+import { council, members } from "@/data/members";
+import { CouncilMemberType, MemberType } from "@/types";
 
 type Props = {};
 
 const Members = (props: Props) => {
   return (
     <>
-      <div className="p-20 font-jetbrains">
-        <h1 className="text-3xl font-extrabold ">Members</h1>
-        <div>
-          <h1 className="text-3xl font-extrabold mt-5 ml-5">The Council</h1>
-          {/* <CouncilCard /> */}
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mt-1">
-            {council.map((member : CouncilMemberType) => (
+      <div className="md:p-20 p-5 font-jetbrains">
+        <div className="">
+          <h1 className="md:text-5xl text-3xl font-extrabold mt-5 md:ml-5 justify-center text-center font-fira_sans">
+            The Council
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mt-1 auto-rows-max ">
+            {council.map((member: CouncilMemberType) => (
               <div key={member.id}>
-                <MemberCard {...member}/>
+                <MemberCard {...member} />
               </div>
             ))}
           </div>
         </div>
         <div>
-          <h1 className="text-3xl font-extrabold ">The Members</h1>
+          <h1 className="md:text-5xl text-3xl font-extrabold mt-5 md:ml-5 justify-center text-center font-fira_sans">
+            The Members
+          </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-1">
-            {members.map((member :MemberType) => (
+            {members.map((member: MemberType) => (
               <div key={member.id}>
                 <MemberCard {...member} />
               </div>
