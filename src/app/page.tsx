f475b802f944553b46fce235a03c45ab83f51e1c
Pranaam19 <22j34.pranam@sjec.ac.in>
2024-06-20 18:41:31 +0530

    Resolved conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 import { DomainDock } from "@/components/DomainDock";
 import { ScrollTextReveal } from "@/components/ScrollTextReveal";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <>
+    <div>
+      <NavBar/>  
       <div className="h-screen border flex justify-center items-center"></div>
       <ScrollTextReveal />
       <div className="h-screen border">About Us</div>
-    <div>
-      <NavBar/>
-
       <div className="h-screen">
         <DomainDock />
       </div>
+    </div>
     </>
   );
 }
