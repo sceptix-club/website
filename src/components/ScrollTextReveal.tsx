8c61c6cc3c78ac9bdd868e82ac79c95f57fb8aae
Pranavv Raja <114171126+pranavvraja@users.noreply.github.com>
2024-06-20 18:31:12 +0530

    update: scrollTextReveal

--- src/components/ScrollTextReveal.tsx
+++ src/components/ScrollTextReveal.tsx
@@ -2,8 +2,10 @@ import TextReveal from "@/components/magicui/text-reveal";
 
 export async function ScrollTextReveal() {
   return (
-    <div className="z-10 flex min-h-min h-min items-center justify-center  bg-white dark:bg-black">
-      <TextReveal text="Liberate the Mind." className="font-fira_sans" />
-    </div>
+    <>
+      <div className="z-10 flex min-h-min h-min items-center justify-center ">
+        <TextReveal text="Liberate the Mind." className="font-fira_sans" />
+      </div>
+    </>
   );
 }
