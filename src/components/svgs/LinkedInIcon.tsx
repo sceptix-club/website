8f0592b3ecb2d0fdf8c22e07b094e86e21529fe1
vyshnav <vyshnavravi599@gmail.com>
2024-06-21 13:36:28 +0530

    feat: Add LinkedInIcon component

---
+++ src/components/svgs/LinkedInIcon.tsx
@@ -0,0 +1,9 @@
+const LinkedInIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5">
+        <path fill="currentColor" d="M19 0h-14c-2.8 0-5 2.2-5 5v14c0 2.8 2.2 5 5 5h14c2.8 0 5-2.2 5-5v-14c0-2.8-2.2-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.5c-.97 0-1.75-.78-1.75-1.75s.78-1.75 1.75-1.75 1.75.78 1.75 1.75-.78 1.75-1.75 1.75zm13.5 11.5h-3v-5c0-1.1-.9-2-2-2s-2 .9-2 2v5h-3v-10h3v1.3c.66-.98 1.81-1.3 2.8-.9s1.5 1.6 1.5 2.8v6.8z"/>
+      </svg>
+    )
+  }
+  
+  export default LinkedInIcon
