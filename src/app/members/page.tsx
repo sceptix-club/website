c5c00d85f4a63622a37f06c2abc6472b3e0efa72
Pranavv Raja <114171126+pranavvraja@users.noreply.github.com>
2024-06-21 13:32:19 +0530

    update: member card and data

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -13,7 +13,7 @@ const Members = (props: Props) => {
           <h1 className="md:text-5xl text-3xl font-extrabold mt-5 md:ml-5 justify-center text-center font-fira_sans">
             The Council
           </h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mt-1 auto-rows-max ">
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mt-1">
             {council.map((member: CouncilMemberType) => (
               <div key={member.id}>
                 <MemberCard {...member} />
@@ -25,7 +25,7 @@ const Members = (props: Props) => {
           <h1 className="md:text-5xl text-3xl font-extrabold mt-5 md:ml-5 justify-center text-center font-fira_sans">
             The Members
           </h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-1">
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mt-1">
             {members.map((member: MemberType) => (
               <div key={member.id}>
                 <MemberCard {...member} />
